ee2e956540fef50c4db59d6b5f7e824ad7569341
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-03-02 09:15:23 +0100

    Creado el backend de Concesionario

---
+++ src/main/java/ies/alcores/backend_concesionario/BackendConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package ies.alcores.backend_concesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConcesionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendConcesionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ies/alcores/backend_concesionario/BackendConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.alcores.backend_concesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConcesionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
